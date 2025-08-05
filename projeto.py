75862d1f5e08b1f877979e50dd0bdde5def45d60
ChavesAmaro <gon94chavesamaro@gmail.com>
2025-08-05 11:33:46 +0100

    New Repo

---
+++ projeto.py
@@ -0,0 +1,143 @@
+import pickle
+from docx import Document
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from datetime import datetime
+import requests
+
+df = pd.DataFrame()
+
+data_inicio = []
+data_final = []
+data_registo = []
+autor = []
+titulo = []
+edicao = []
+paginas = []
+preco = []
+tema_principal = []
+avaliacao = []
+link_google = []
+
+
+def obter_link_google_books(titulo, autor, api_key):
+    query = f"{titulo} {autor}"
+    url = f"https://www.googleapis.com/books/v1/volumes?q={query}&key={api_key}"
+    response = requests.get(url)
+    data = response.json()
+    
+    if "items" in data:
+        livro = data["items"][0]["volumeInfo"]
+        return livro.get("infoLink", "Link não disponível")
+    else:
+        return "Livro não encontrado"
+
+def adicionar_lista():
+    global df
+    loop_registo = True
+    while loop_registo == True:
+        try:
+            inicio_d = input("Indique quando começou a ler o livro (formato DD/MM/YYYY): ")
+            data_i = datetime.strptime(inicio_d, "%d/%m/%Y")
+            fim_d = input("Indique quando terminou de ler o livro (formato DD/MM/YYYY) ou deixe em branco caso ainda não tenha terminado: ").strip()
+            if fim_d:
+                data_f = datetime.strptime(fim_d, "%d/%m/%Y")
+            else:
+                data_f = "Em Leitura"
+            data_r = datetime.now().strftime("%d-%m-%Y %H:%M:%S")
+            autor_r = input("Indique o autor do livro a registar: ")
+            titulo_r = input("Indique o título do livro: ")
+            preco_r = float(input("Indique o preço do livro: "))
+            edicao_r = int(input("Indique a edição do livro: "))
+            paginas_r = int(input("Indique o número de páginas do livro: "))
+            tema_r = input("Indique o tema principal do livro: ")
+            
+            loop_avaliacao = True
+            while loop_avaliacao == True:
+                try:
+                    avaliacao_r = float(input("Indique a sua avaliação do livro (0-5)"))
+                    
+                    if 0 <= avaliacao_r <= 5:
+                        loop_avaliacao = False
+                    else:
+                        print("Erro! A avaliação deve estar entre 0 e 5.")
+                except ValueError:
+                    print("Erro! Introduza um número real na avaliação.")
+            
+            data_inicio.append(data_i)
+            data_final.append(data_f)
+            data_registo.append(data_r)
+            autor.append(autor_r)
+            titulo.append(titulo_r)
+            preco.append(preco_r)
+            edicao.append(edicao_r)
+            paginas.append(paginas_r)
+            tema_principal.append(tema_r)
+            avaliacao.append(avaliacao_r)
+            
+            
+            print("Livro registado com sucesso!")
+        except ValueError:
+            print("Erro! O preco deve ser um valor numérico")
+        
+        api_key = ""
+        link_google_books = obter_link_google_books(titulo_r, autor_r, api_key)
+        link_google.append(link_google_books)
+
+        loop_verificacao = True
+        while loop_verificacao == True:
+            continuar_registar = input("Deseja registar mais um livro? (s/n): ").strip().lower()
+            
+            if continuar_registar in ['s', 'n']:
+                loop_verificacao = False
+            else:
+                print("Erro! Resposta inválida introduza 's' para sim ou 'n' para não.")
+        
+        if continuar_registar == 'n':
+            loop_registo = False
+    
+    df = pd.DataFrame({
+        "Data do Início da Leitura": data_inicio,
+        "Data do Fim da Leitura": data_final,
+        "Data de Registo": data_registo,
+        "Autor": autor,
+        "Título": titulo,
+        "Preço": preco,
+        "Edicao": edicao,
+        "Páginas": paginas,
+        "Tema Principal": tema_principal,
+        "Avaliação Pessoal": avaliacao,
+        "Link Google Books": link_google
+    })
+    
+def consultar_lista():
+    global df
+    if df.empty:
+        print("A lista está vazia!")
+        return
+    print("Lista de livros")
+    print(df)
+    print()
+
+def main():
+    loop_menu = True
+    while loop_menu == True:
+        print("\nMenu Principal")
+        print("1. Adicionar Livro à Lista")
+        print("2. Consultar Lista de Livros")
+        print("3. Sair")
+
+        escolha_menu_principal = input("\nEscolha uma opção de 1 a 3: ").strip()
+        
+        if escolha_menu_principal == '1':
+            adicionar_lista()
+        elif escolha_menu_principal == '2':
+            consultar_lista()
+        elif escolha_menu_principal == '3':
+            print("\nA encerrar o programa...")
+            loop_menu = False
+        else:
+            print("Erro! Escolha um número de 1 a 3.")
+
+main()
