a91220f7922b4d6cae2ed2e15e0e556c11011e24
ChavesAmaro <gon94chavesamaro@gmail.com>
2025-08-05 14:42:30 +0100

    Bug fixes
    foram tratados vários problemas de formatação no dataframe

--- projeto.py
+++ projeto.py
@@ -19,6 +19,7 @@ data_final = []
 data_registo = []
 autor = []
 titulo = []
+ano_publicacao = []
 edicao = []
 paginas = []
 preco = []
@@ -47,16 +48,23 @@ def adicionar_lista():
     loop_registo = True
     while loop_registo == True:
         try:
-            inicio_d = input("Indique quando começou a ler o livro (formato DD/MM/YYYY): ")
-            data_i = datetime.strptime(inicio_d, "%d/%m/%Y")
+            inicio_d = input("\nIndique quando começou a ler o livro (formato DD/MM/YYYY) ou deixe em branco caso ainda não tenha começado: ").strip()
+            if inicio_d:
+                data_i = datetime.strptime(inicio_d, "%d/%m/%Y")
+            else:
+                data_i = "Por ler"
             fim_d = input("Indique quando terminou de ler o livro (formato DD/MM/YYYY) ou deixe em branco caso ainda não tenha terminado: ").strip()
             if fim_d:
                 data_f = datetime.strptime(fim_d, "%d/%m/%Y")
+            elif data_i == "Por ler":
+                data_f = "Por ler"
             else:
                 data_f = "Em Leitura"
             data_r = datetime.now().strftime("%d-%m-%Y %H:%M:%S")
             autor_r = input("Indique o autor do livro a registar: ")
             titulo_r = input("Indique o título do livro: ")
+            ano_p = input("Indique o ano de publicação do livro: ")
+            ano_p_r = datetime.strptime(ano_p, "%Y")
             preco_r = float(input("Indique o preço do livro: "))
             edicao_r = int(input("Indique a edição do livro: "))
             paginas_r = int(input("Indique o número de páginas do livro: "))
@@ -65,7 +73,7 @@ def adicionar_lista():
             loop_avaliacao = True
             while loop_avaliacao == True:
                 try:
-                    avaliacao_r = float(input("Indique a sua avaliação do livro (0-5)"))
+                    avaliacao_r = float(input("Indique a sua avaliação do livro (0-5): "))
                     
                     if 0 <= avaliacao_r <= 5:
                         loop_avaliacao = False
@@ -79,6 +87,7 @@ def adicionar_lista():
             data_registo.append(data_r)
             autor.append(autor_r)
             titulo.append(titulo_r)
+            ano_publicacao.append(ano_p_r)
             preco.append(preco_r)
             edicao.append(edicao_r)
             paginas.append(paginas_r)
@@ -88,14 +97,14 @@ def adicionar_lista():
             
             print("Livro registado com sucesso!")
         except ValueError:
-            print("Erro! O preco deve ser um valor numérico")
+            print("Erro! Formato dos valores errado!")
         
         link_google_books = obter_link_google_books(titulo_r, autor_r)
         link_google.append(link_google_books)
 
         loop_verificacao = True
         while loop_verificacao == True:
-            continuar_registar = input("Deseja registar mais um livro? (s/n): ").strip().lower()
+            continuar_registar = input("\nDeseja registar mais um livro? (s/n): ").strip().lower()
             
             if continuar_registar in ['s', 'n']:
                 loop_verificacao = False
@@ -105,12 +114,13 @@ def adicionar_lista():
         if continuar_registar == 'n':
             loop_registo = False
     
-    df = pd.DataFrame({
+    df_novo = pd.DataFrame({
         "Data do Início da Leitura": data_inicio,
         "Data do Fim da Leitura": data_final,
         "Data de Registo": data_registo,
         "Autor": autor,
         "Título": titulo,
+        "Ano de Publicação": ano_publicacao,
         "Preço": preco,
         "Edicao": edicao,
         "Páginas": paginas,
@@ -118,14 +128,38 @@ def adicionar_lista():
         "Avaliação Pessoal": avaliacao,
         "Link Google Books": link_google
     })
-    
+    df = pd.concat([df, df_novo], ignore_index=True)
+
+    data_inicio.clear()
+    data_final.clear()
+    data_registo.clear()
+    autor.clear()
+    titulo.clear()
+    ano_publicacao.clear()
+    edicao.clear()
+    paginas.clear()
+    preco.clear()
+    tema_principal.clear()
+    avaliacao.clear()
+    link_google.clear()
+      
 def consultar_lista():
     global df
     if df.empty:
-        print("A lista está vazia!")
+        print("\nA lista está vazia!")
         return
+    
+    df_formatado = df.copy()
+    
+    df_formatado["Data do Início da Leitura"] = df_formatado["Data do Início da Leitura"].apply(
+        lambda x: x.strftime("%d/%m/%Y") if isinstance(x, datetime) else x)
+    df_formatado["Data do Fim da Leitura"] = df_formatado["Data do Fim da Leitura"].apply(
+        lambda x: x.strftime("%d/%m/%Y") if isinstance(x, datetime) else x)
+    df_formatado["Ano de Publicação"] = df_formatado["Ano de Publicação"].apply(
+        lambda x: x.strftime("%Y") if isinstance(x, datetime) else x)
+    
     print("Lista de livros")
-    print(df)
+    print(df_formatado)
     print()
     
 def exportar_excel():
@@ -135,12 +169,21 @@ def exportar_excel():
         return
     
     nome_ficheiro = input("Introduza o nome do ficheiro a guardar: ")
-    
     if not nome_ficheiro.endswith('.xlsx'):
         nome_ficheiro += '.xlsx'
     
     try:
-        df.to_excel(nome_ficheiro, index=False)
+        df_formatado = df.copy()
+        
+        df_formatado["Data do Início da Leitura"] = df_formatado["Data do Início da Leitura"].apply(
+        lambda x: x.strftime("%d/%m/%Y") if isinstance(x, datetime) else x)
+        df_formatado["Data do Fim da Leitura"] = df_formatado["Data do Fim da Leitura"].apply(
+        lambda x: x.strftime("%d/%m/%Y") if isinstance(x, datetime) else x)
+        df_formatado["Ano de Publicação"] = df_formatado["Ano de Publicação"].apply(
+        lambda x: x.strftime("%Y") if isinstance(x, datetime) else x)
+        
+        df_formatado.to_excel(nome_ficheiro, index=False)
+        
         print(f"Dados exportados com sucesso para: {nome_ficheiro}")
     except Exception as erroexportexcel:
         print(f"Erro ao gerar ficheiro: {erroexportexcel}")
@@ -170,14 +213,22 @@ def export_pdf():
         return
     
     nome_arquivo = input("Introduza o nome do ficheiro PDF a guardar: ")
-    if not nome_ficheiro.endswith('.pdf'):
-        nome_ficheiro += '.pdf'
+    if not nome_arquivo.endswith('.pdf'):
+        nome_arquivo += '.pdf'
     
     
     doc = SimpleDocTemplate(nome_arquivo, pagesize=A4)
     elementos = []
-
-    dados = [df.columns.tolist()] + df.values.tolist()
+    df_formatado = df.copy()
+        
+    df_formatado["Data do Início da Leitura"] = df_formatado["Data do Início da Leitura"].apply(
+    lambda x: x.strftime("%d/%m/%Y") if isinstance(x, datetime) else x)
+    df_formatado["Data do Fim da Leitura"] = df_formatado["Data do Fim da Leitura"].apply(
+    lambda x: x.strftime("%d/%m/%Y") if isinstance(x, datetime) else x)
+    df_formatado["Ano de Publicação"] = df_formatado["Ano de Publicação"].apply(
+    lambda x: x.strftime("%Y") if isinstance(x, datetime) else x)
+    
+    dados = [df_formatado.columns.tolist()] + df.values.tolist()
     tabela = Table(dados)
     
     
@@ -185,8 +236,8 @@ def export_pdf():
         ('BACKGROUND', (0, 0), (-1, 0), colors.lightblue),
         ('TEXTCOLOR', (0, 0), (-1, 0), colors.black),
         ('ALIGN', (0, 0), (-1, -1), 'LEFT'),
-        ('FONTNAME', (0, 0), (-1, 0), 'Arial'),
-        ('FONTSIZE', (0, 0), (-1, -1), 8),
+        ('FONTNAME', (0, 0), (-1, 0), 'Helvetica'),
+        ('FONTSIZE', (0, 0), (-1, -1), 4),
         ('BOTTOMPADDING', (0, 0), (-1, 0), 6),
         ('GRID', (0, 0), (-1, -1), 0.5, colors.grey),
     ])
@@ -204,9 +255,10 @@ def main():
         print("2. Consultar Lista de Livros")
         print("3. Exportar a Lista Para Ficheiro .xlsx")
         print("4. Importar a Lista de Ficheiro .xlsx")
-        print("5. Sair")
+        print("5. Exportar para .pdf")
+        print("6. Sair")
 
-        escolha_menu_principal = input("\nEscolha uma opção de 1 a 5: ").strip()
+        escolha_menu_principal = input("\nEscolha uma opção de 1 a 6: ").strip() #mudar no final para o nº de opções
         
         if escolha_menu_principal == '1':
             adicionar_lista()
@@ -217,9 +269,14 @@ def main():
         elif escolha_menu_principal == '4':
             importar_excel()
         elif escolha_menu_principal == '5':
+            export_pdf()
+        elif escolha_menu_principal == '6':
             print("\nA encerrar o programa...")
             loop_menu = False
         else:
-            print("Erro! Escolha um número de 1 a 5.")
+            print("Erro! Escolha um número de 1 a 6.") #mudar no final para o nº de opções
 
-main()
+if __name__ == "__main__":
+    print("\n\n")
+    main()
+    print("\n\n")
